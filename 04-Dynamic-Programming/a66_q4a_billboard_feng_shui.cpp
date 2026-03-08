c5970b6c0d268f1863b111c44edcd97f79a75fca
Sukrit Suwanichwong <sukrit.sww@gmail.com>
2026-03-08 19:02:08 +0700

    code initialization

---
+++ 04-Dynamic-Programming/a66_q4a_billboard_feng_shui.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> billboards(n+1);
+    vector<int> pattern(k);
+    for (int i=1; i<n+1; i++) cin >> billboards[i];
+    for (int i=0; i<k; i++) cin >> pattern[i];
+    
+}
