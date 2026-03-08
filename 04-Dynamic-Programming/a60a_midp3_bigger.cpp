6a563d8325399350b5a6e6b019d0904d79c8d0fb
Sukrit Suwanichwong <sukrit.sww@gmail.com>
2026-03-08 12:42:48 +0700

    code initialization (input part)

---
+++ 04-Dynamic-Programming/a60a_midp3_bigger.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n; //number of billboard
+    cin >> n;
+    vector<int> billboards(n+1);
+    for (int i=1; i<n+1; i++) cin >> billboards[i];
+
+    
+}
