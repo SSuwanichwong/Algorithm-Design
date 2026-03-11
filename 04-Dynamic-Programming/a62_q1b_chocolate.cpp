759867dca9844011456eb693b718c80ab1ebf8c1
Sukrit Suwanichwong <sukrit.sww@gmail.com>
2026-03-11 10:48:04 +0700

    feat: add dynamic programming

--- 04-Dynamic-Programming/a62_q1b_chocolate.cpp
+++ 04-Dynamic-Programming/a62_q1b_chocolate.cpp
@@ -11,5 +11,23 @@ int main() {
     vector<int> s(k);
     for (int i=0; i<k; i++) cin >> s[i];
 
+    vector<int> dp(n+1, 0); // dp[i] is the amount of method we can split for i pieces
+    
+    //base case
+    dp[0] = 1; //0 pieces one method is don't eat
+
+    for (int i=1; i<n+1; i++){
+        for (auto &y : s) {
+            if (i-y<0) continue;
+
+            dp[i] += dp[i-y] % x; // eat y pieces check at i-y
+        }
+        dp[i] %= x;
+    }
+
+    cout << dp[n] << "\n";
+
+
+
 
 }
