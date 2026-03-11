6ea668ec4f25c5958c8c41c9df67388f70911bb2
Sukrit Suwanichwong <sukrit.sww@gmail.com>
2026-03-11 10:19:09 +0700

    code initialization

---
+++ 04-Dynamic-Programming/a62_q1b_chocolate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int x = 1000003;
+
+int main() {
+    int n, k; // n pieces size of set S
+    cin >> n >> k;
+
+    vector<int> s(k);
+    for (int i=0; i<k; i++) cin >> s[i];
+
+
+}
