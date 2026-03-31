ed28f4597fa67e84303f4dd937144f419f062e28
Sukrit Suwanichwong <sukrit.sww@gmail.com>
2026-03-31 11:31:46 +0700

    clean up the 4 ways walking to be cleaner

--- 05-Graph/ex05e1_shortest.cpp
+++ 05-Graph/ex05e1_shortest.cpp
@@ -22,26 +22,20 @@ int main() {
 int bfs(int R, int C, pair<int, int> start, vector<vector<int>> &distance){
     queue<pair<int,int>> q;
     q.push(start);
+    //4 ways to walk (up, down, left, right)
+    vector<int> dr = {-1, 1, 0, 0};
+    vector<int> dc = {0, 0, -1, 1};
     while (!q.empty()) {
         int r = q.front().first, c = q.front().second;
         if (r == R-1 && c == C-1) break;
         q.pop();
-        //4 ways to walk (up, down, left, right)
-        if (c+1 < C && distance[r][c+1] == 0){
-            distance[r][c+1] = distance[r][c] + 1;
-            q.push({r,c+1});
-        }
-        if (r+1 < R && distance[r+1][c] == 0){
-            distance[r+1][c] = distance[r][c] + 1;
-            q.push({r+1,c});
-        } 
-        if (r-1 >= 0 && distance[r-1][c] == 0){
-            distance[r-1][c] = distance[r][c] + 1;
-            q.push({r-1,c});
-        }
-        if (c-1 >= 0 && distance[r][c-1] == 0){
-            distance[r][c-1] = distance[r][c] + 1;
-            q.push({r,c-1});
+        for (int i = 0; i < 4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (0 <= nr && nr < R && 0 <= nc && nc < C && distance[nr][nc] == 0){
+                distance[nr][nc] = distance[r][c] + 1;
+                q.push({nr,nc});
+            }
         }
     }
     if (distance[R-1][C-1] != 0) return distance[R-1][C-1];
