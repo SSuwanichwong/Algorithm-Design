808f18819f06d5de5a20dc1197f60d30280de757
Sukrit Suwanichwong <sukrit.sww@gmail.com>
2026-03-31 15:03:14 +0700

    feat: complete

---
+++ 02-Sorting/a68_q2a_dividing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int,int>> spica;
+    int spica_sum = 0, rigil_sum = 0;
+    for (int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        spica.push_back({x, i+1});
+        spica_sum += x;
+    } 
+
+    sort(spica.rbegin(), spica.rend());
+
+    while (!spica.empty() && spica_sum - rigil_sum > 2 * spica.back().first) {
+        int x = spica.back().first;
+        spica_sum -= x;
+        rigil_sum += x;
+        spica.pop_back();
+    }
+    if (spica.size() == 0) return -1;
+    cout << spica.size() << "\n";
+    for (auto &s : spica) cout << s.second << " ";
+    cout << endl;
+}
